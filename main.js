a4455f6eca54b74a7bc056d779d6e8098d319538
Florencia Pagella <flo.pagella94@hotmail.com>
2023-11-10 17:05:31 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("input");
+const addbtn = document.querySelector(".btn_add");
+const ul= document.querySelector("ul");
+const empty = document.querySelector(".empty");
+
+addbtn.addEventListener('click', (e)) => {
+    
+}
