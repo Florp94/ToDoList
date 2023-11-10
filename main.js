d21c731da620f4a4b1a119a881a95c53b3f0fe13
Florencia Pagella <flo.pagella94@hotmail.com>
2023-11-10 17:40:34 -0300

    estilado

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const addbtn = document.querySelector(".btn_add");
 const ul= document.querySelector("ul");
 const empty = document.querySelector(".empty");
 
-addbtn.addEventListener('click', (e)) => {
-    
-}
+addbtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text = input.value;
+
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = text;
+
+    li.appendChild(p);
+    ul.appendChild(li);
+})
